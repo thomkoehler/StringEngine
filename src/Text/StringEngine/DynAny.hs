3ba8233181d5a595001610923470db33f0848f5b
thomkoehler <thom.koehler@gmail.com>
2015-07-17 13:42:33 +0200

    StringTemplate

--- src/Text/StringEngine/DynAny.hs
+++ src/Text/StringEngine/DynAny.hs
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------------------------
 
-{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, OverlappingInstances #-}
 
 module Text.StringEngine.DynAny where
 
